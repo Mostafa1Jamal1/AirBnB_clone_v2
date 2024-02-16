1ea27d83d611a9cb3c7bc5efd3ebd2273e86ed4f
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-02-16 08:11:41 +0200

    style: remove a trailing whitespace

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    ''' display “C” followed by the value of the text 
+    ''' display “C” followed by the value of the text
     and replace _ with space on route /c/<text> '''
     return f"C {text.replace('_', ' ')}"
 
