5bc79a913169bd2206ed51462e45c8459873035b
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-02-16 08:10:04 +0200

    task 1 in 0x04.AirBnB clone-Web framework project

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' a script that starts a Flask web application '''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    ''' display “Hello HBNB!” on route / '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    ''' display “HBNB” on route /hbnb '''
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
