d519ecc428a1981459a676db0527e02ebebc524f
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-02-19 00:03:01 +0200

    feat: deploying database engine

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    # HBNB_TYPE_STORAGE can be “file” (FileStorage) or db (DBStorage)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', back_populates="state")
+    else:
+        @property
+        def cities(self):
+            ''' FileStorage relationship between State and City
+            returns the list of City instances with state_id
+            equals to the current State.id'''
+            from models import storage
+            from models.city import City
+            city_list = {}
+            for key, city in storage.all(City).items():
+                if self.id == city.state_id:
+                    city_list[key] = city
+            return city_list
