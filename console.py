70656a83b91e4d202f655c40b8fbc350559c8952
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-02-19 03:08:24 +0200

    refact: do_all method

--- console.py
+++ console.py
@@ -241,11 +241,10 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage.all():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+            for v in storage.all(HBNBCommand.classes[args]).values():
+                print_list.append(str(v))
         else:
-            for k, v in storage.all():
+            for v in storage.all().values():
                 print_list.append(str(v))
 
         print(print_list)
